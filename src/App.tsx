7b7ef999ea66b47b979636ef0fab208b236d10b1
Sudip <sudbrl@gmail.com>
2026-07-19 20:44:57 +0545

    s

--- src/App.tsx
+++ src/App.tsx
@@ -177,7 +177,7 @@ export default function App() {
                         ))}
                         </Pie>
                         <Tooltip
-                          formatter={(value: number) => formatCurrency(value)}
+			formatter={(value) => formatCurrency(Number(value ?? 0))}
                           contentStyle={{
                             borderRadius: "0.5rem",
                             border: "1px solid #e2e8f0",
